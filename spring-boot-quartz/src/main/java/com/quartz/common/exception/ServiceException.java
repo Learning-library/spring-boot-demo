c3bb9f8cd0166aae08d0c7f9f3841f7fa474bba0
changwenhu <wenhu.chang@smartapp4u.com>
2017-05-16 22:18:26 +0800

    spring-boot and quartz

--- spring-boot-quartz/src/main/java/com/quartz/common/exception/ServiceException.java
+++ spring-boot-quartz/src/main/java/com/quartz/common/exception/ServiceException.java
@@ -2,9 +2,8 @@ package com.quartz.common.exception;
 
 /**
  * 自定义针对service异常
- *
- * @author mrchang
- *         2015年5月27日 下午5:09:01
+ * @author lance
+ * 2015年5月27日 下午5:09:01
  */
 public class ServiceException extends RuntimeException {
     private static final long serialVersionUID = 8624944628363400977L;
@@ -16,6 +15,7 @@ public class ServiceException extends RuntimeException {
     public ServiceException(String message, Throwable cause,
                             boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
+//	    super(message, cause, enableSuppression, writableStackTrace);
     }
 
     public ServiceException(String message, Throwable cause) {
